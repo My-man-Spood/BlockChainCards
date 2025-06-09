d15f06a42c124587bc1b13eebbe6fc5437a0509d
jeremie <ownedstuff@hotmail.fr>
2025-06-08 20:40:29 -0400

    further implementation of block file reader

--- Spood.BlockChainCards.Lib/Utils/ByteArrayExtensions.cs
+++ Spood.BlockChainCards.Lib/Utils/ByteArrayExtensions.cs
@@ -6,5 +6,16 @@
         {
             return Convert.ToHexString(bytes);
         }
+
+        /// <summary>
+        /// Returns the length of the array as a little-endian byte array
+        /// </summary>
+        public static byte[] LengthBytesLE(this byte[] data)
+        {
+            var lengthBytes = BitConverter.GetBytes(data.Length);
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(lengthBytes);
+            return lengthBytes;
+        }
     }
 }
--- Spood.BlockChainCards.Serialization/BlockFileReader.cs
+++ Spood.BlockChainCards.Serialization/BlockFileReader.cs
@@ -1,6 +1,9 @@
 namespace Spood.BlockChainCards.Serialization;
 
+using System.Security.Principal;
 using Spood.BlockChainCards.Lib;
+using Spood.BlockChainCards.Lib.Utils;
+
 public class BlockFileReader
 {
     private readonly string filePath;
@@ -10,43 +13,66 @@ public class BlockFileReader
         this.filePath = filePath;
     }
 
-    public BCBlock ReadBlock(int blockIndex)
+    public string GetOpenBlockFilePath()
     {
+        var files = Directory.GetFiles(filePath, "_*.blk");
+        return files[^1];
     }
 
-    public int GetLastBlockIndex()
+    public void AppendBlock(byte[] blockData)
     {
+        var openBlockPath = GetOpenBlockFilePath();
+        int blockCount = AppendBlockToFile(openBlockPath, blockData);
+        RotateBlockFileIfFull(openBlockPath, blockCount);
     }
 
-    public void AppendBlock(byte[] blockData, string filePath)
+    private static int AppendBlockToFile(string filePath, byte[] blockData)
     {
-        int blockLength = blockData.Length;
-        using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Write, FileShare.None))
+        using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Write, FileShare.None);
+        WriteBlockToEnd(stream, blockData);
+        int newCount = IncrementBlockCount(stream);
+        return newCount;
+    }
+
+    private static void WriteBlockToEnd(FileStream stream, byte[] blockData)
+    {
+        stream.Seek(0, SeekOrigin.End);
+        stream.Write(blockData.LengthBytesLE(), 0, 4);
+        stream.Write(blockData, 0, blockData.Length);
+    }
+
+    private static int IncrementBlockCount(FileStream stream)
+    {
+        stream.Seek(0, SeekOrigin.Begin);
+        var countBuffer = new byte[4];
+        stream.Read(countBuffer, 0, 4);
+        int count = BitConverter.ToInt32(countBuffer, 0) + 1;
+        stream.Seek(0, SeekOrigin.Begin);
+        stream.Write(BitConverter.GetBytes(count), 0, 4);
+        stream.Flush();
+        return count;
+    }
+
+    private static void RotateBlockFileIfFull(string openBlockPath, int blockCount)
+    {
+        if (blockCount == 1000)
         {
-            // Write the block length as a 4-byte integer
-            byte[] lengthBytes = BitConverter.GetBytes(blockLength);
-            if (!BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(lengthBytes);
-            }
-            stream.Seek(0, SeekOrigin.End); // Move to the end of the file
-            stream.Write(lengthBytes, 0, lengthBytes.Length);
-            // Write the block data
-            stream.Write(blockData, 0, blockData.Length);
-
-            // Go back to start of the file and increment block count
-            stream.Seek(0, SeekOrigin.Begin);
-            var blockCountBuffer = new byte[4];
-            stream.Read(blockCountBuffer, 0, blockCountBuffer.Length);
-            int blockCount = BitConverter.ToInt32(blockCountBuffer, 0);
-            blockCount++;
-            if (!BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(blockCountBuffer);
-            }
-            stream.Seek(0, SeekOrigin.Begin); // Move to the start of the file
-            stream.Write(BitConverter.GetBytes(blockCount), 0, 4); // Write the updated block count
-            stream.Flush();
+            CloseBlock(openBlockPath);
+            CreateNewBlock(openBlockPath);
         }
+    }
+
+    private static void CloseBlock(string openBlockPath)
+    {
+        File.Move(openBlockPath, openBlockPath.Replace("_", ""));
+    }
+
+    private static void CreateNewBlock(string openBlockPath)
+    {
+        var newBlockNumber = int.Parse(openBlockPath.Replace("_", "")) + 1;
+        using var filestream = File.Create($"_{newBlockNumber:D6}.blk");
+
+        // Write initial block count (0)
+        filestream.Write(BitConverter.GetBytes(0), 0, 4);
     }
 }
